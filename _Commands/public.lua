a7b1d4d7785d6faa6037545299e2d0a35fa832d6
Xinshou <ShahriaFahim@live.com>
2021-07-14 23:48:46 +0600

    added Quote command

--- _Commands/public.lua
+++ _Commands/public.lua
@@ -2,3 +2,29 @@ CMD["ping"] = function(message)
     message:reply("pong")
 end
 
+CMD["quote"] = function(message)
+    local _, arg = parseMsg(message.content, 1)
+    if tonumber(arg[1]) == nil then 
+        message:reply("It must be snowflake ID") return end
+    
+    local msgChannel = message.mentionedChannels.first or message.channel
+
+    local  Msg = msgChannel:getMessage(arg[1])
+    if not Msg then
+        message:reply("Couldn't find the quote you looking for.") return end
+
+    message:reply{
+        embed = {
+            author = {
+                name = Msg.author.username,
+                icon_url = Msg.author.avatarURL
+            },
+            description = "```"..Msg.content.."```",
+            footer = {
+                text = "#".. msgChannel.name
+            },
+            timestamp = Msg.timestamp,
+        }
+    }
+end
+
