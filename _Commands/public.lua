0394549fc648bb52839b8a451fbb833a45f9c39a
Xinshou <ShahriaFahim@live.com>
2021-08-02 18:31:29 +0600

    added +reddit Subreddit, ez

--- _Commands/public.lua
+++ _Commands/public.lua
@@ -26,11 +26,51 @@ CMD["quote"] = function(message)
         }
     }
 end
+
 --Currently only taking image photo, will add more features soon!
 CMD["promeme"] = function(message)
     ::redo::
     local uri = "https://www.reddit.com/r/ProgrammerHumor/random/.json"
     local Suc, Res = http.request("GET", uri)
+    local body = decode(Res)[1].data.children[1]
+    local data = body.data
+    --
+    if data.is_video then
+        goto redo
+    return end
+    message.channel:send{
+        embed = {
+            title = data.title,
+            description = data.selftext,
+            image = {
+                url = data.url_overridden_by_dest
+            },
+            thumbnail = {
+                url = data.thumbnail
+            },
+            author = {
+                name = data.author,
+                url = "https://www.reddit.com"..data.permalink
+            }
+        },
+        reference = { message = message, mention = true}
+    }
+end
+
+
+CMD["reddit"] = function(message)
+    ::redo::
+    local _, arg = parseMsg(message, 1)
+    local uri = "https://www.reddit.com/r/"..arg[1].."/random/.json"
+    local Suc, Res = http.request("GET", uri)
+
+    if not Suc then
+        goto redo
+    return end
+    
+   if decode(Res).error == 404 then
+        message:reply("No sub-reddit found, try again.")
+    return end
     local body = decode(Res)[1].data.children[1]
     local data = body.data
     --
